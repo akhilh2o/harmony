ea8769814a8f09064e81584a63d2aa5829690a30
akhilh2o <d.akhil.gupta@com>
2025-07-27 11:22:16 +0530

    fix route

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,6 @@ use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    Artisan::call('storage:link');
     Artisan::call('optimize:clear');
-    Artisan::call('config:cache');
-    Artisan::call('route:cache');
-    Artisan::call('view:cache');
-    Artisan::call('migrate', ['--force' => true]);
     return view('welcome');
 });
