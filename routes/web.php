020e4d4dc0e0ee0f8056cd2582888d80db8b217b
BablooKannojia <bablookmr143@gmail.com>
2026-04-20 18:14:45 +0530

    livewire web

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,11 @@
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
+// Route::get('/', function () {
+//     Artisan::call('optimize:clear');
+//     return view('welcome');
+// });
+
 Route::get('/', function () {
-    Artisan::call('optimize:clear');
-    return view('welcome');
+    return redirect('/admin/login');
 });
